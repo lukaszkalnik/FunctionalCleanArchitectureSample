2980cffb120d8c5529eadb78f59499cd7e039e20
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2020-02-11 11:47:09 +0100

    Add explanatory comment to DefaultGatewayApi factory method

--- app/src/main/kotlin/com/lukaszkalnik/functionalcleanarchitecture/GatewayApiService.kt
+++ app/src/main/kotlin/com/lukaszkalnik/functionalcleanarchitecture/GatewayApiService.kt
@@ -30,6 +30,18 @@ interface GatewayApiService {
 
         /**
          * A factory function to create an instance of [GatewayApiService] for calling from the client code.
+         *
+         * Note that this is also where the central dependency injection is done.
+         *
+         * This function creates a [GatewayApi] instance from the data layer, resolves the converters and injects them into the
+         * use case factory functions to create use case instances.
+         *
+         * It then creates an instance of its own implementation, [DefaultGatewayApiService], injecting created
+         * use cases. This instance is then returned to the caller.
+         *
+         * This dependency graph resolution might be done equally well outside of this class. You might have placed it
+         * in an external singleton "injector" file or even used a DI framework for this. It is located here just
+         * for namespacing.
          */
         fun with(baseUrl: String): GatewayApiService {
             val api = GatewayApi.create(baseUrl)
